77427faa5eff523bd95235f3e6b7829ede2baa23
Tylam.dev <tylam.dev@gmail.com>
2024-03-26 13:54:46 -0500

    change the url http for https

--- src/api/Index.jsx
+++ src/api/Index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const getData = (place) => {
-    return axios.get(`http://api.weatherapi.com/v1/forecast.json?key=def8d1526e724975820160722242203&q=${place}`)
+    return axios.get(`https://api.weatherapi.com/v1/forecast.json?key=def8d1526e724975820160722242203&q=${place}`)
   }
 
 export default getData
